2e4c48a22ddfc519d6383e54457f799484f901ee
Amanda <amandamaiken@berkeley.edu>
2022-01-10 21:16:30 -0800

    commit tree b6fe6c8c74ef0714c11c0da2953be504659fab54

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(newCustomer){
+  if ( newCustomer <= 400 ) {
+    return 'This one is on me!'
+  } else {
+      if ( newCustomer > 2500 ) {
+        return 'No can do.'
+      } else if ( newCustomer >= 2000 ) {
+        return 'I will gladly take your thirty bucks.'
+      }
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  return destination === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(customerService){
+  switch(customerService){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'notGenerous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
